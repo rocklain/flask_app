90255e8f85dcb2553d641c18935ed784362eabe7
rocklain <ztgk.3142@gmail.com>
2025-07-28 00:34:25 +0900

    DB登録

---
+++ init_db.py
@@ -0,0 +1,10 @@
+from company_blog import db, app
+from company_blog.models import User
+
+with app.app_context():
+    db.create_all()
+
+    user1 = User(email="admin_user@test.com",
+                username="Admin User", password="123", administrator="1")
+    db.session.add(user1)
+    db.session.commit()
