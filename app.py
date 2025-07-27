50597bf338921b6d0084336fa6e7525ce8717901
rocklain <ztgk.3142@gmail.com>
2025-07-27 21:49:07 +0900

    forms.pyの切り出し

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import render_template, url_for, redirect, session, flash, request, abort
-from flask_wtf import FlaskForm
-from wtforms import ValidationError, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from sqlalchemy.engine import Engine
@@ -13,45 +10,10 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('ログイン')
 
 
-class RegistrationForm(FlaskForm):
-    email = StringField('メールアドレス', validators=[DataRequired(), Email(message='正しいメールアドレスを入力してください')])
-    username = StringField('ユーザー名', validators=[DataRequired()])
-    password = PasswordField('パスワード', validators=[DataRequired(), EqualTo('pass_confirm', message='パスワードが一致していません')])
-    pass_confirm = PasswordField('パスワード(確認)', validators=[DataRequired()])
-    submit = SubmitField('登録')
 
-    def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError('入力されたユーザー名は既に使われています。')
 
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('入力されたメールアドレスは既に登場されています。')
-
-class UpdateUserForm(FlaskForm):
-    email = StringField('メールアドレス', validators=[DataRequired(), Email()])
-    username = StringField('ユーザー名', validators=[DataRequired()])
-    password = PasswordField('パスワード', validators=[EqualTo('pass_confirm', message='パスワードが一致していません。')])
-    pass_confirm = PasswordField('パスワード(確認)')
-    submit = SubmitField('更新')
-
-    def __init__(self, user_id, *args, **kwargs):
-        super(UpdateUserForm, self).__init__(*args, **kwargs)
-        self.id = user_id
-
-    def validate_email(self, field):
-        if User.query.filter(User.id != self.id).filter_by(email=field.data).first():
-            raise ValidationError('入力されたメールアドレスは既に登録されています。')
-
-    def validate_username(self, field):
-        if User.query.filter(User.id != self.id).filter_by(username=field.data).first():
-            raise ValidationError('入力されたユーザー名は既に使われています。')
 
 @app.errorhandler(403)
 def error_403(error):
