6894dce5ade9a793003a0db829097a1b02825080
rocklain <ztgk.3142@gmail.com>
2025-07-27 21:44:26 +0900

    initial commit

---
+++ company_blog/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+def localize_callback(*args, **kwargs):
+    return 'このページにアクセスするには、ログインが必要です。'
+login_manager.localize_callback = localize_callback
---
+++ company_blog/models.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from pytz import timezone
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+
+from company_blog import db, login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    administrator = db.Column(db.String(1))
+    post = db.relationship('BlogPost', backref='author', lazy='dynamic')
+
+    def __init__(self, email, username, password, administrator):
+        self.email = email
+        self.username = username
+        self.password = password
+        self.administrator = administrator
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def is_administrator(self):
+        if self.administrator == "1":
+            return 1
+        else:
+            return 0
+
+class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/Tokyo')))
+    title = db.Column(db.String(140))
+    text = db.Column(db.Text)
+    summary = db.Column(db.String(140))
+    featured_image = db.Column(db.String(140))
+
+    def __init__(self, title, text, featured_image, user_id, summary):
+        self.title = title
+        self.text = text
+        self.featured_image = featured_image
+        self.user_id = user_id
+        self.summary = summary
+
+    def __repr__(self):
+        return f"PostID: {self.id}, Title: {self.title}, Author: {self.author} \n"
